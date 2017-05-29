cd1cecb275a7e160dfce722ba98b159df16a4ae7
Unknown <laurent.esteve@soprasteria.com>
2017-05-29 18:16:35 +0200

    Ajout de la page erreur au rendu + redirection avec le filtre

--- src/com/sopra/filter/ConnexionFilter.java
+++ src/com/sopra/filter/ConnexionFilter.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sopra.Constantes;
+import com.sopra.model.Rendu;
+import com.sopra.servlet.action.ErreurServlet;
 
 /**
  * Servlet Filter implementation class connexionFilter
@@ -84,7 +86,7 @@ public class ConnexionFilter implements Filter {
 			if (request.getSession().getAttribute(Constantes.username) == null) {
 									
 				// accès non autorisé !
-				response.sendRedirect("erreur");
+				Rendu.pageErreur(request.getServletContext(), request, response);
 				return;
 			}
 			

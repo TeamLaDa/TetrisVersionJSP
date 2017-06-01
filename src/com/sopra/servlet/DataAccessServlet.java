a7a5c3880f1c7aa5788edcec3eeb88fdd3e0ffd9
Unknown <laurent.esteve@soprasteria.com>
2017-06-01 17:01:59 +0200

    MàJ du Data Access Servlet

--- src/com/sopra/servlet/DataAccessServlet.java
+++ src/com/sopra/servlet/DataAccessServlet.java
@@ -6,7 +6,12 @@ import javax.servlet.http.HttpServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import com.sopra.dao.IBlocDAO;
 import com.sopra.dao.IDAO;
+import com.sopra.dao.IFigureDAO;
+import com.sopra.dao.IPartieDAO;
+import com.sopra.dao.IScoreDAO;
+import com.sopra.dao.ITetriminosDAO;
 import com.sopra.dao.IUtilisateurDAO;
 import com.sopra.model.Tetrimino;
 
@@ -17,10 +22,23 @@ public abstract class DataAccessServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Autowired
-	protected IDAO<Tetrimino, String> tetriminosDao;
+	protected ITetriminosDAO tetriminosDao;
+	
+	@Autowired
+	protected IBlocDAO blocDAO;
+	
+	@Autowired 
+	IFigureDAO figureDAO;
 
 	@Autowired
 	protected IUtilisateurDAO utilisateurDao;
+	
+	@Autowired 
+	IScoreDAO scoreDAO;
+	
+	@Autowired
+	IPartieDAO partieDAO;
+	
 
 	
 	// surcharge de la méthode init, permettant de faire un pont entre Spring et

7bc566f23c0be3241f9293d7e9198210d28e919a
Unknown <laurent.esteve@soprasteria.com>
2017-06-06 14:10:11 +0200

    Ajout de requetes lazy loading dans les DAO Figure et Bloc

--- src/com/sopra/dao/IBlocDAO.java
+++ src/com/sopra/dao/IBlocDAO.java
@@ -1,7 +1,9 @@
 package com.sopra.dao;
 
+import java.util.List;
+
 import com.sopra.model.Bloc;
 
 public interface IBlocDAO extends IDAO<Bloc, Integer> {
-
+	public List<Bloc> findWithFigure(int id_figure);
 }
--- src/com/sopra/dao/hibernate/BlocHibernateDAO.java
+++ src/com/sopra/dao/hibernate/BlocHibernateDAO.java
@@ -2,6 +2,10 @@ package com.sopra.dao.hibernate;
 
 
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +18,12 @@ public class BlocHibernateDAO extends DAOHibernate<Bloc, Integer> implements IBl
 	public BlocHibernateDAO() {
 		this.maClasse = Bloc.class;
 		this.maClasseString = "Bloc";
+	}
+
+	@Override
+	public List<Bloc> findWithFigure(int id_figure) {
+		Query myQuery = em.createQuery("from Bloc b left join fetch b.figure f where f.id = :id_figure", this.maClasse);
+		myQuery.setParameter("id_figure", id_figure);		
+		return myQuery.getResultList();
 	}
 }
